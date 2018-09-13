c31f1161c926a08523f9b93c7e05a0626a146e6d
Thomas Gabauer <tgabauer@gmail.com>
2018-09-13 13:53:36 -0700

    Part 1 complete

--- part1/server.js
+++ part1/server.js
@@ -2,9 +2,30 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.get('/hello', function(req, res) {
+  res.send("Hello!");
 });
+
+app.post('/create/:name', function(req, res) {
+  var myReturn = {
+    "id":1,
+    "name":`${req.params.name}`
+  }
+  res.json(myReturn);
+});
+
+app.get('/', function(req, res) {
+  res.sendFile("/Users/tomgabauer/galvanize/expressIntroChallenges/part1/index.html");
+});
+
+app.get('/verify/:age', function(req, res) {
+  if (req.params.age > 13) {
+    res.sendStatus('200')
+  }
+  res.sendStatus("403");
+});
+
+
 
 app.use(function(req, res) {
   res.sendStatus(404);
